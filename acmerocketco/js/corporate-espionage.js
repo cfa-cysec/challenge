80450b391e8a1027a16ca56a0962e4a7966d5e4a
DiasS <36014967+0xdiass@users.noreply.github.com>
2024-12-13 13:31:15 +0000

    add 2 challenges

---
+++ acmerocketco/js/corporate-espionage.js
@@ -0,0 +1,25 @@
+// alert('Do you have what it takes to hack the Acme Rocket Co. store front? You\'ve heard rumors they are working on a new model called the Javelin Rocket Model b Series 3 the details are set to be released in the next month. Do you think you can hack their site and get the scoop before anyone else does?');
+
+function instructionsForHacking() {
+  let updateButon = document.getElementById('updateDetails');
+  let cancelButton = document.getElementById('cancel');
+  let dialog = document.getElementById('favDialog');
+  dialog.returnValue = 'favAnimal';
+
+  function openCheck(dialog) {
+    if (dialog.open) { console.log('Dialog open'); }
+    else { console.log('Dailog closed'); }
+  }
+  //help button opens a modal
+  updateButon.addEventListener('click', function () {
+    dialog.showModal();
+    openCheck(dialog);
+  });
+  //add cancel button to close modal dialog box
+  cancelButton.addEventListener('click', function () {
+    dialog.closest('animalNotChosen');
+    openCheck(dialog);
+  });
+}
+
+instructionsForHacking();
